6d49203273d44b9381567c9174b6509d5133b882
Alexander Meißner <AlexanderMeissner@gmx.net>
2024-04-11 22:11:05 +0200

    Replaces distutils by setuptools.

--- Makefile
+++ Makefile
@@ -2,13 +2,16 @@ HEADER = $(wildcard include/*.h)
 SRC = $(wildcard src/*.c)
 
 python_build: $(SRC) $(HEADER) setup.py
-	python setup.py build
+	python3 -m build
 
 python_install: python_build
-	python setup.py install
+	python3 -m pip install .
 
-python_deploy: python_build
-	python setup.py sdist upload -r pypi
+python_pack:
+	python3 -m build --sdist
+
+python_deploy: python_pack
+	python3 -m twine upload dist/*
 
 shared: $(SRC) $(HEADER)
 	$(CC) src/ccwt.c src/render_png.c -Iinclude -o libccwt.so -std=c99 -Os -fPIC -shared -lm -lpthread -lfftw3 -lfftw3_threads -lpng
--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 #coding=UTF-8
 
-from distutils.core import setup, Extension
-import numpy
+from setuptools import Extension, setup
+import platform, subprocess, numpy
 
-name = 'ccwt'
-version = '0.0.7'
-url = 'https://github.com/lichtso/ccwt'
+include_dirs = ["include", numpy.get_include()]
+library_dirs = []
 
-import os
-os.environ['ARCHFLAGS'] = '-arch x86_64'
+if platform.system() == "Darwin":
+    p = subprocess.run(["which", "-s", "brew"])
+    if p.returncode == 0:
+        include_dirs.append("/opt/homebrew/include/")
+        library_dirs.append("/opt/homebrew/lib/")
+
 setup(
-    name = name,
-    version = version,
-    license = 'MIT Licence',
-    description = 'Complex continuous wavelet transformation',
-    platforms = ['Linux', 'Mac OS-X', 'Unix'],
-    author = 'Alexander Meißner',
-    author_email = 'AlexanderMeissner@gmx.net',
-    url = url,
-    download_url = url+'/tarball/v'+version,
-    ext_modules = [Extension(name,
-        language = 'c',
-        extra_compile_args = ['-std=c99'],
-        include_dirs = ['include', numpy.get_include()],
-        libraries = ['pthread', 'fftw3', 'fftw3_threads', 'png'],
-        sources = ['src/ccwt.c', 'src/render_png.c', 'src/python_api.c']
+    ext_modules = [Extension("ccwt",
+        language = "c",
+        extra_compile_args = ["-std=c99"],
+        include_dirs = include_dirs,
+        library_dirs = library_dirs,
+        libraries = ["pthread", "fftw3", "fftw3_threads", "png"],
+        sources = ["src/ccwt.c", "src/render_png.c", "src/python_api.c"]
     )]
 )
